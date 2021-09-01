e74ae8b59db3d47f1ac0d2b360c0e1735a54c544
Augustine Adjei <austinejei@gmail.com>
2021-09-01 13:45:51 +0000

    --removed deprecated SDK methods/classes

--- hubtelapi-dotnet-v1/Hubtel/ApiList.cs
+++ hubtelapi-dotnet-v1/Hubtel/ApiList.cs
@@ -36,42 +36,9 @@ namespace hubtelapi_dotnet_v1.Hubtel
                                 _items.Add((T) Convert.ChangeType(new Action(o.ToObject<ApiDictionary>()), typeof (T)));
                         }
                         break;
-                    case "campaignlist":
-                        var array = jso[key] as IEnumerable;
-                        if (array != null) {
-                            foreach (JObject o in array)
-                                _items.Add((T) Convert.ChangeType(new Campaign(o.ToObject<ApiDictionary>()), typeof (T)));
-                        }
-                        break;
-                    case "libraries":
-                        var os = jso[key] as IEnumerable;
-                        if (os != null) {
-                            foreach (JObject o in os)
-                                _items.Add((T) Convert.ChangeType(new ContentLibrary(o.ToObject<ApiDictionary>()), typeof (T)));
-                        }
-                        break;
-                    case "contactlist":
-                        var apiArray1 = jso[key] as IEnumerable;
-                        if (apiArray1 != null) {
-                            foreach (JObject o in apiArray1)
-                                _items.Add((T) Convert.ChangeType(new Contact(o.ToObject<ApiDictionary>()), typeof (T)));
-                        }
-                        break;
-                    case "grouplist":
-                        var array1 = jso[key] as IEnumerable;
-                        if (array1 != null) {
-                            foreach (JObject o in array1)
-                                _items.Add((T) Convert.ChangeType(new ContactGroup(o.ToObject<ApiDictionary>()), typeof (T)));
-                        }
-                        break;
+                   
 
-                    case "invoicestatementlist":
-                        var os1 = jso[key] as IEnumerable;
-                        if (os1 != null) {
-                            foreach (JObject o in os1)
-                                _items.Add((T) Convert.ChangeType(new Invoice(o.ToObject<ApiDictionary>()), typeof (T)));
-                        }
-                        break;
+                 
                     case "messages":
                         var apiArray2 = jso[key] as IEnumerable;
                         if (apiArray2 != null) {
@@ -79,64 +46,8 @@ namespace hubtelapi_dotnet_v1.Hubtel
                                 _items.Add((T) Convert.ChangeType(new Message(o.ToObject<ApiDictionary>()), typeof (T)));
                         }
                         break;
-                    case "messagetemplatelist":
-                        var array2 = jso[key] as IEnumerable;
-                        if (array2 != null) {
-                            foreach (JObject o in array2)
-                                _items.Add((T) Convert.ChangeType(new MessageTemplate(o.ToObject<ApiDictionary>()), typeof (T)));
-                        }
-                        break;
-                    case "mokeywordlist":
-                        var os2 = jso[key] as IEnumerable;
-                        if (os2 != null) {
-                            foreach (JObject o in os2)
-                                _items.Add((T) Convert.ChangeType(new MoKeyWord(o.ToObject<ApiDictionary>()), typeof (T)));
-                        }
-                        break;
-                    case "numberplanlist":
-                        var apiArray3 = jso[key] as IEnumerable;
-                        if (apiArray3 != null) {
-                            foreach (JObject o in apiArray3)
-                                _items.Add((T) Convert.ChangeType(new NumberPlan(o.ToObject<ApiDictionary>()), typeof (T)));
-                        }
-                        break;
-                    case "senderaddresseslist":
-                        var array3 = jso[key] as IEnumerable;
-                        if (array3 != null) {
-                            foreach (JObject o in array3)
-                                _items.Add((T) Convert.ChangeType(new Sender(o.ToObject<ApiDictionary>()), typeof (T)));
-                        }
-                        break;
-                    case "ticketlist":
-                        var array4 = jso[key] as IEnumerable;
-                        if (array4 != null) {
-                            foreach (JObject o in array4)
-                                _items.Add((T) Convert.ChangeType(new Ticket(o.ToObject<ApiDictionary>()), typeof (T)));
-                        }
-                        break;
-                    case "servicelist":
-                        var os3 = jso[key] as IEnumerable;
-                        if (os3 != null) {
-                            foreach (JObject o in os3) {
-                                var d = o.ToObject<ApiDictionary>();
-                                _items.Add((T) Convert.ChangeType(new Service(d), typeof (T)));
-                            }
-                        }
-                        break;
-                    case "folders":
-                        var arr = jso[key] as IEnumerable;
-                        if (arr != null) {
-                            foreach (JObject o in arr)
-                                _items.Add((T) Convert.ChangeType(new ContentFolder(o.ToObject<ApiDictionary>()), typeof (T)));
-                        }
-                        break;
-                    case "medias":
-                        var arr1 = jso[key] as IEnumerable;
-                        if (arr1 != null) {
-                            foreach (JObject o in arr1)
-                                _items.Add((T) Convert.ChangeType(new ContentMedia(o.ToObject<ApiDictionary>()), typeof (T)));
-                        }
-                        break;
+                  
+                   
                 }
             }
         }
